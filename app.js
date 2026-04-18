ba401a8ca27a1198b987c5351ce5eb85e876e0a8
Toluwani Erijesu <toluwanierijesu@gmail.com>
2026-04-18 22:36:39 +0100

    your message

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-const hamburger = document.getElementById('hamburger');
+/*const hamburger = document.getElementById('hamburger');
 const mobileMenu = document.getElementById('mobileMenu');
 
 hamburger.addEventListener('click', () => {
@@ -8,10 +7,24 @@ hamburger.addEventListener('click', () => {
 });
 
 
-console.log("Hello world"[8]);
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.getElementById('hamburger');
+  const mobileMenu = document.getElementById('mobileMenu');
+  const mainNav = document.getElementById('mainNav');
+
+  hamburger.addEventListener('click', function () {
+    const isOpen = mobileMenu.classList.toggle('open');
+    hamburger.classList.toggle('open', isOpen);
+
+    // fixes border-radius when menu is open
+    mainNav.classList.toggle('menu-closed', !isOpen);
+  });
+});*/
+
+console.log("Hello world"[5]);
 console.log("23433232323");
 console.log(`Ted` + `Code`);
-console.log("ASAP".length);
+console.log("ASAP".length); /* the length of a string*/
 
 /*Numbers the normal calculations are done, however signs like % will give the remainder the / sign will divide perfectly. 10%3= 1, 9%3=0 no remainders. 10/2=5*/
 
@@ -19,8 +32,9 @@ console.log("ASAP".length);
 let string = "TEDCODE";
 console.log(string[6]);
 
+// Let Keyword in variable
 let userEmail = "erijesu@gmail.com"; /* the form in writing variables*/
-+
+
 /*let age = 25;
 let name = "Erijesu";
 
@@ -31,15 +45,16 @@ console.log(age);*/
 const points = 200;
 console.log(points);
 
-// creating a converter from USD to AUD
+// creating a currency converter from USD to AUD
 let USD = 1200;
 let AUD = USD * 1.5;
-console.log(AUD);
-// comparison Operators
-let comparison = 5 == "5";
+console.log(USD);
+
+// comparison Operators......... equality comparison operators
+let comparison = 5 == "5"; // double equals checks for equal values
 console.log(comparison);
 //  mostly used
-let comparisonOperators = "5" === 7;
+let comparisonOperators = "5" === 5; // checks for equal values and types
 console.log(comparisonOperators);
 
 // ****conditionals***//
@@ -63,9 +78,47 @@ else {
   console.log("you are  ${age} yr old, a minor please leave.");
 }*/
 
-let age = 18;
-let hasId = true;
+// Tenary Operators
+let isRaining = true;
 
-if (age >= 18 && hasId === true) {
-  console.log(`you may enter the club`);
+isRaining ? console.log("it is raining") : console.log("it is not raining");
+
+let age = 19;
+let hasId = true;
+//let str = age >= 19 && hasId ? "You may enter" : "you may not enter";
+//console.log(str);
+
+let loggedIn = true;
+let hasMember = false;
+
+//let str = loggedIn && hasMember ? "show the video" : "dont show the video";
+//console.log(str);
+
+// **** LOOPS *** //
+// DRY == Don't repeat yourself
+
+//let counter = 1;
+
+//while (counter <= 20) {
+//  console.log(counter);
+// counter++;
+//}
+//console.log(" while loop finished running");
+
+//for (let i = 0; i < 50; i++) {
+// console.log(i);
+//}
+
+//excersise
+
+for (let i = 1; i <= 15; ++i) {
+  if (i % 5 === 0) {
+    console.log("${i} - Asap  frontend");
+  } else if (i % 2 === 0) {
+    console.log("${i} - frontend");
+  } else {
+    console.log("${i} - Asap");
+  }
 }
+
+let str = "ASAP Frontend";
